df0f439e5d32e68dea63afecbf98d5c665b46e4b
oxkoma <113691237+oxkoma@users.noreply.github.com>
2023-05-16 13:15:54 +0300

    Add files via upload

---
+++ database/migrations/2023_05_09_095139_create_menu_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('action');
+            $table->string('content');
+            $table->string('keywords');
+            $table->string('descriptions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
